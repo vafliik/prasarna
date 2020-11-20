68351e27fcc5ec6ae703147e48f44ce344c38cbf
Vojtěch Červený <cervik49@gmail.com>
2020-11-20 15:09:45 +0100

    clear project :D

--- jest-puppeteer.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  launch: {
-    headless: false,
-    slowMo: 50,
-    defaultViewport: null,
-    devtools: true,
-    args: ['--disable-dev-shm-usage', '--shm-size=2gb']
-  }
-};
